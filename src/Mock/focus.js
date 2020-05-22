f15d06b8f54833672fd33d16e8c8a67141076c68
fengziye <1224971566@qq.com>
2020-05-22 14:39:07 +0800

    remove useless

--- src/Mock/focus.js
+++ src/Mock/focus.js
@@ -10,20 +10,17 @@ export default [
                     {
                         id: 1,
                         title: 'Akina',
-                        img: 'https://s1.ax1x.com/2020/05/14/YDfRnU.jpg',
-                        href: 'https://zhebk.cn/Web/Akina.html'
+                        img: 'https://s1.ax1x.com/2020/05/14/YDfRnU.jpg'
                     },
                     {
                         id: 2,
                         title: '使用说明',
-                        img: 'https://s1.ax1x.com/2020/05/14/YDf4AJ.jpg',
-                        href: 'https://zhebk.cn/userAkina.html'
+                        img: 'https://s1.ax1x.com/2020/05/14/YDf4AJ.jpg'
                     },
                     {
                         id: 3,
                         title: '文章归档',
-                        img: 'https://s1.ax1x.com/2020/05/14/YDfT91.jpg',
-                        href: 'https://zhebk.cn/archives.html'
+                        img: 'https://s1.ax1x.com/2020/05/14/YDfT91.jpg'
                     }
                 ]
             }
