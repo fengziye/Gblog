e1cbb19393ad967ea21082fcb9b511483f0a80d4
fengziye <1224971566@qq.com>
2020-06-04 11:26:22 +0800

    add seo;refine page title

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
   <img width="80" src="http://cdn.fengziy.cn/gblog/logo.svg"/>
 </p>
 <p align="center">
+  <a href="https://gitee.com/fengziy/Gblog">
+    <img src="https://gitee.com/fengziy/Gblog/badge/star.svg?theme=white" alt="vue"/>
+  </a>
+  <a href="https://gitee.com/fengziy/Gblog">
+      <img src="https://img.shields.io/github/stars/fengziye/Gblog.svg?style=social&label=Stars" alt="vue"/>
+  </a>
   <a href="https://github.com/vuejs/vue">
     <img src="https://img.shields.io/badge/vue-2.6.11-brightgreen.svg" alt="vue"/>
   </a>
--- src/router/index.js
+++ src/router/index.js
@@ -8,32 +8,38 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: () => import('../views/Home.vue')
+        component: () => import('../views/Home.vue'),
+        meta: { title: '首页'}
     },
     {
         path: '/category/:cate',
         name: 'category',
-        component: () => import('../views/Home.vue')
+        component: () => import('../views/Home.vue'),
+        meta: { title: '分类', params: 'cate'}
     },
     {
         path: '/search/:words',
         name: 'search',
-        component: () => import('../views/Home.vue')
+        component: () => import('../views/Home.vue'),
+        meta: { title: '搜索', params: 'words'}
     },
     {
         path: '/about',
         name: 'about',
-        component: () => import('../views/About.vue')
+        component: () => import('../views/About.vue'),
+        meta: { title: '关于'}
     },
     {
         path: '/friend',
         name: 'friend',
-        component: () => import('../views/Friend.vue')
+        component: () => import('../views/Friend.vue'),
+        meta: { title: '友链'}
     },
     {
         path: '/article/:id',
         name: 'article',
-        component: () => import('../views/Articles.vue')
+        component: () => import('../views/Articles.vue'),
+        meta: { title: '文章'}
     }
 ]
 
@@ -43,6 +49,13 @@ const router = new VueRouter({
     routes
 })
 router.beforeEach((to, from, next) => {
+    let title = 'Gblog'
+    if (to.meta.params){
+        title = `${to.meta.title}:${to.params[to.meta.params] || ''} - ${title}`
+    }else {
+        title = `${to.meta.title} - ${title}`
+    }
+    document.title = title
     store.dispatch('setLoading', true);
     next();
 })
